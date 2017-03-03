9edf13897b0793d77ea740fd069b48948aa3656c
Jahongir Rahmonov  =) <jrahmonov2@gmail.com>
2017-03-03 10:20:31 +0500

    Configure circle ci

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker build -t jahongirr/sample-node .
+docker push jahongirr/sample-node
+
+ssh deploy@159.203.127.59 << EOF
+docker pull jahongirr/sample-node:latest
+docker stop web || true
+docker rm web || true
+docker rmi jahongirr/sample-node:current || true
+docker tag jahongirr/sample-node:latest jahongirr/sample-node:current
+docker run -d --net app --restart always --name web -p 3000:3000 jahongirr/sample-node:current
+EOF
